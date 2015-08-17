f71e2e9ca898f35213da66549e5c96f5474fd042
unknown <amercati@tai.it>
2015-08-17 17:52:41 +0200

    working on i18n

--- src/main/java/org/merka/showcase/user/TestPageController.java
+++ src/main/java/org/merka/showcase/user/TestPageController.java
@@ -1,14 +1,28 @@
 package org.merka.showcase.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.merka.showcase.utils.ShowcaseUtils.*;
+
+import java.util.Locale;
 
 import org.merka.showcase.BasePageController;
 
 @Controller
 public class TestPageController extends BasePageController
 {
+	@Autowired
+	MessageSource messageSource;
+	
+	@ModelAttribute("testMessage")
+	public String getTestMessage(Locale locale)
+	{
+		return messageSource.getMessage("test.message", null, locale);
+	}
+	
 	@RequestMapping(path = "/probe")
 	public String getTestPage()
 	{
